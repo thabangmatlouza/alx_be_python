cd9ae3af598aabd54eb3cce144a9d3125e07aba6
Thabang <patolebekoholdings@gmail.com>
2025-08-03 23:52:16 +0200

    Final: passing unit tests for SimpleCalculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -16,12 +16,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(0, 0), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(0, 10), 0)
         self.assertEqual(self.calc.multiply(-1, 5), -5)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(6, 3), 2)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
         self.assertEqual(self.calc.divide(5, 0), None)  # division by zero case
