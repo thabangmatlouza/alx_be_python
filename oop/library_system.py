8b729dab0fb3f06e46781894516543f259f904cb
Thabang <patolebekoholdings@gmail.com>
2025-08-05 08:50:11 +0200

    Add __str__ methods for ALX inheritance and composition task

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author
 
-    def details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
@@ -12,7 +12,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -21,7 +21,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
@@ -30,10 +30,8 @@ class Library:
         self.books = []
 
     def add_book(self, book):
-        # Accepts Book or subclasses of Book
         self.books.append(book)
 
     def list_books(self):
         for book in self.books:
-            print(book.details())
-
+            print(book)
