7e0bfaec8ce8fb5c56a21f19ca2e88627bf5907d
Thabang <patolebekoholdings@gmail.com>
2025-09-10 08:47:37 +0200

    feat: add shopping_list_manager with add, remove, view functionality

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,29 +14,26 @@ def main():
         choice = input("Enter your choice: ").strip()
 
         if choice == '1':
-            # Add an item
             item = input("Enter the item to add: ").strip()
             if item:
                 shopping_list.append(item)
-                print(f"'{item}' has been added to your shopping list.")
+                print("'{}' has been added to your shopping list.".format(item))
             else:
                 print("You must enter a valid item.")
 
         elif choice == '2':
-            # Remove an item
             item = input("Enter the item to remove: ").strip()
             if item in shopping_list:
                 shopping_list.remove(item)
-                print(f"'{item}' has been removed from your shopping list.")
+                print("'{}' has been removed from your shopping list.".format(item))
             else:
-                print(f"'{item}' was not found in your shopping list.")
+                print("'{}' was not found in your shopping list.".format(item))
 
         elif choice == '3':
-            # View the shopping list
             if shopping_list:
                 print("Your current shopping list:")
                 for index, item in enumerate(shopping_list, start=1):
-                    print(f"{index}. {item}")
+                    print("{}. {}".format(index, item))
             else:
                 print("Your shopping list is empty.")
 
