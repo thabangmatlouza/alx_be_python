729cacb0d30213b56d5649d39f84b383f489c856
Thabang <patolebekoholdings@gmail.com>
2025-10-19 11:33:21 +0200

    Reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,13 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time_bound? (yes/no): ").lower()
+
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"Reminder: {task} is high priority task that requires immediate attention today! ")
+        else:    
+            print(f"Reminder: {task} is low priority. Consider completing it when you have free time. ")   
+
+    case _:
+        print("Unexpected input. ")
